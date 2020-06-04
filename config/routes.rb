2ca535662f72f637c194bb8e5b204590c4f6f682
DoubleLama <lama31@hotmail.fr>
2020-06-04 12:06:22 +0200

    versioning ok

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  devise_for :users,
+  path: '',
+  path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  }     
+
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :images do
+        resources :comments
+       end
+    end
+  end
+end
